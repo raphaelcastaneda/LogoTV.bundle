e8d33083f57f45c02d701755bbd018b29bb4c784
shopgirl284 <shopgirl284@gmail.com>
2014-05-06 19:52:12 -0400

    updated code for new url service

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -6,10 +6,11 @@ ICON = 'icon-default.png'
 BASE_URL = 'http://www.logotv.com'
 SHOWS = 'http://www.logotv.com/shows/'
 VIDEOS = 'http://www.logotv.com/video/showall.jhtml'
-MRSS_URL = "http://www.logotv.com/player/includes/rss.jhtml?uri=mgid:uma:videolist:logotv.com:%s"
-
+PLAYLIST_URL = 'http://www.logotv.com/video/?id='
+RE_EP_SEASON  = Regex('Episode (\d{1,3}), Season (\d{1,2})')
 RE_EPISODE  = Regex('[-/]episode-(\d{1,3})[-/]')
 RE_SEASON  = Regex('-season-(\d{1,2})-')
+ANDROID_EXCLUSION  = ['full-episodes', 'full-movies', 'other-series']
 
 # ADDTIONAL LINKS FOUND THAT COULD BE USED
 # http://www.logotv.com/video/modules/more.jhtml?id=1705856&vid=undefined
@@ -46,6 +47,8 @@ def ProduceCarousels(title, url):
   data = HTML.ElementFromURL(url)
   for video in data.xpath('//div[@class="carousel_section_container"]'):
     show_type = video.xpath('.//@id')[0]
+    if Client.Platform in ('Android') and show_type in ANDROID_EXCLUSION:
+      continue
     # The video page has an extra h2 in code and must be put first or title comes up blank
     try:
       title = video.xpath('.//div[@class="carousel_section_title"]/h2//text()')[0].strip()
@@ -77,7 +80,7 @@ def ProduceShows(title):
       if not url.startswith('http://www.logotv.com'):
         continue
     title = video.xpath('.//text()')[0]
-    oc.add(DirectoryObject(key=Callback(ShowVideos, title=title, url=url), title = title))
+    oc.add(DirectoryObject(key=Callback(ShowSections, title=title, url=url), title = title))
 
   if len(oc) < 1:
     Log ('still no value for objects')
@@ -107,82 +110,77 @@ def MoreVideos(title, url, show_type):
       thumb = BASE_URL + thumb
       
     if vid_url.endswith('series.jhtml'):
-      oc.add(DirectoryObject(key=Callback(ShowVideos, title=title, url=vid_url), title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
-    elif vid_url.endswith('playlist.jhtml'):
+      oc.add(DirectoryObject(key=Callback(ShowSections, title=title, url=vid_url, thumb=thumb), title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
+    else:
       date = Datetime.ParseDate(video.xpath('./div[@class="addedDate"]//text()')[0])
       if 'movies' in show_type:
         oc.add(VideoClipObject(url = vid_url, title = title, originally_available_at = date, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
       else:
         # All appear to have an episode but some do not have a season but put both in a try/except to prevent any issues
-        try:
-          episode = int(RE_EPISODE.search(vid_url).group(1))
-        except:
-          episode = 0
-        try:
-          season = int(RE_SEASON.search(vid_url).group(1))
-        except:
-          season = 1
+        try: episode = int(RE_EPISODE.search(vid_url).group(1))
+        except: episode = 0
+        try: season = int(RE_SEASON.search(vid_url).group(1))
+        except: season = 0
         oc.add(EpisodeObject(url = vid_url, title = title, index = episode, season = season, originally_available_at = date, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
-    else:
-      if '?id' in vid_url:
-        vid_id = vid_url.split('?id=')[1]
-        rss_url = MRSS_URL %vid_id
-        # send to videopage function
-        oc.add(DirectoryObject(key=Callback(RSSPage, title=title, url=rss_url, thumb=thumb), title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
-      else:
-        pass
 
   if len(oc) < 1:
     Log ('still no value for objects')
     return ObjectContainer(header="Empty", message="There are no videos to list right now.")
   else:
     return oc
+#####################################################################################
+# For Producing All Shows list at bottom of show page
+@route(PREFIX + '/showsections')
+def ShowSections(title, url, thumb=R(ICON)):
+  oc = ObjectContainer(title2=title)
+  if Client.Platform not in ('Android'):
+    oc.add(DirectoryObject(key=Callback(ShowVideos, title="Full Episodes", url=url, vid_type='Full', image=thumb), title = "Full Episodes", thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
+  oc.add(DirectoryObject(key=Callback(ShowVideos, title="Other Videos", url=url, image=thumb), title = "Bonus Clips", thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
+  return oc
 #################################################################################################################
 # This function produces videos from the table layout used by show video pages
 # This function picks up all videos in all pages even without paging code
-# Full episodes and movies have a value for @mainuri
 @route(PREFIX + '/showvideos')
-def ShowVideos(title, url):
+def ShowVideos(title, url, image=R(ICON), vid_type='Bonus'):
   oc = ObjectContainer(title2=title)
-  data = HTML.ElementFromURL(url)
-  for video in data.xpath('//ol/li[@itemtype="http://schema.org/VideoObject"]'):
-    # Logo has extra section of same videos that are picked up so put it in a try
-    try:
-      title = video.xpath('.//@maintitle')[0]
-    except:
+  try: data = HTML.ElementFromURL(url)
+  except: return ObjectContainer(header=L('Error'), message=L('Unable to access data for this show. Webpage URL no longer valid'))
+  for video in data.xpath('//ol[@id="olListing"]/li[@itemtype="http://schema.org/VideoObject"]'):
+    title = video.xpath('./@maintitle')[0]
+    content_type = video.xpath('.//li[@class="list-ct"]//text()')[0]
+    vid_content = content_type.split()[0]
+    if vid_content != vid_type:
       continue
-    thumb = video.xpath('./meta[@itemprop="thumbnail"]//@content')[0].split('?')[0]
-    if not thumb.startswith('http://'):
+    thumb = video.xpath('./meta[@itemprop="thumbnail"]/@content')[0].split('?')[0]
+    thumb = thumb.replace('70x53.jpg', '510x340.jpg')
+    if not thumb:
+      try: thumb = BASE_URL + video.xpath('.//*[@itemprop="thumbnail"]/@src')[0].split('?')[0]
+      except: thumb = image
+    else:
       thumb = BASE_URL + thumb
-    vid_url = BASE_URL + video.xpath('.//@mainurl')[0]
-    desc = video.xpath('.//@maincontent')[0]
-    date = video.xpath('.//@mainposted')[0]
+    vid_url = BASE_URL + video.xpath('./@mainurl')[0]
+    desc = video.xpath('./@maincontent')[0]
+    date = video.xpath('./@mainposted')[0]
     if 'hrs ago' in date:
       date = Datetime.Now()
     else:
       date = Datetime.ParseDate(date)
-    content = video.xpath('./ul/li[@class="list-ct"]//text()')[0]
-    if content:
-      title = '%s - %s' %(content, title)
-    if content != 'Full Episode':
-      if '#id=' in vid_url:
-      # Since video clip playlist are of varying size and impossible to determine parts in URL service, have to do so in code
-      # Most of Logos other videos are playlist with as many as 30 videos, so check for it and produce full list here
-        vid_id = vid_url.split('#id=')[1]
-        rss_url = MRSS_URL %vid_id
-        oc.add(DirectoryObject(key=Callback(RSSPage, title=title, url=rss_url, thumb=thumb), title=title, thumb=thumb))
-      else:
-        # if movie or doc in url, no season or episode number, so create a movie object
-        oc.add(MovieObject(url = vid_url, title = title, thumb = Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON),
-          originally_available_at = date, summary = desc))
+    if content_type == 'Full Movies':
+      # if movie or doc in url, no season or episode number, so create a movie object
+      oc.add(MovieObject(url = vid_url, title = title, thumb = Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON),
+        originally_available_at = date, summary = desc))
     else:
+      if '#id=' in vid_url:
+      # Since video clips for Logo are usually playlist, we change these to playlist urls that are recognized by url service 
+        vid_id = vid_url.split('#id=')[1]
+        vid_url = PLAYLIST_URL + vid_id
       episode = video.xpath('.//li[@class="list-ep"]//text()')[0]
-      try:
+      if episode.isdigit()==True:
         season = int(episode[0])
         episode = int(episode)
-      except:
-        episode = 0
-        season = 0
+      else:
+        try: (episode, season) = (int(RE_EP_SEASON.search(title).group(1)), int(RE_EP_SEASON.search(title).group(2)))
+        except: (episode, season) =  (0, 0)
 
       oc.add(EpisodeObject(
         url = vid_url, 
@@ -193,36 +191,9 @@ def ShowVideos(title, url):
         originally_available_at = date,
         summary = desc
       ))
-  #oc.objects.sort(key = lambda obj: obj.originally_available_at, reverse=True)
 
   if len(oc) < 1:
     Log ('still no value for objects')
     return ObjectContainer(header="Empty", message="There are no videos to list right now.")
-  else:
-    return oc
-####################################################################################################
-# This produces videos video playlist broken down with the RSS feed
-# The url for thumbs produced from these RSS feed pages give errors and is not resolved with fallback, so just using prior thumb sent to function
-@route(PREFIX + '/rsspage')
-def RSSPage(url, title, thumb):
-  oc = ObjectContainer(title2=title)
-  data = XML.ElementFromURL(url)
-  for item in data.xpath('//item'):
-    link = item.xpath('./link//text()')[0]
-    #image = item.xpath('./image//@url')[0]
-    video_title = item.xpath('./title//text()')[0]
-    date = item.xpath('./pubDate//text()')[0]
-    if 'hrs ago' in date:
-      try:
-        date = Datetime.Now()
-      except:
-        date = ''
-    else:
-      date = Datetime.ParseDate(date)
-
-    oc.add(VideoClipObject(url=link, title=video_title, originally_available_at=date, thumb=thumb))
-
-  if len(oc)==0:
-    return ObjectContainer(header="Sorry!", message="No video available in this category.")
   else:
     return oc
